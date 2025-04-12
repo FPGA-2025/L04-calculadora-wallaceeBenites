178547f93c11b0ea835e4a50ea14961702d389a3
wallaceeBenites <wallacepuck@gmail.com>
2025-04-12 19:26:46 -0300

    teste

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,18 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+reg [7:0] resultado;
+
+always @(*) begin
+  case (codigo)
+    3'b000: resultado = entrada_A + entrada_B; 
+    3'b001: resultado = entrada_A - entrada_B; 
+    3'b010: resultado = entrada_A;             
+    3'b011: resultado = entrada_B;             
+    default: resultado = 8'b00000000;          
+  endcase
+end
+
+assign saida = resultado;
 
 endmodule
