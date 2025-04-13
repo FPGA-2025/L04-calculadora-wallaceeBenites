eafd011cc3c361186a460ecb70480a2b670e4c8b
wallaceeBenites <wallacepuck@gmail.com>
2025-04-13 19:40:34 -0300

    teste

--- calculadora.v
+++ calculadora.v
@@ -1,22 +1,17 @@
 module calculadora(
-  input   [7:0] entrada_A,
-  input   [7:0] entrada_B,
-  input   [2:0] codigo,
-  output  [7:0] saida
+    input  [7:0] entrada_A,
+    input  [7:0] entrada_B,
+    input  [2:0] codigo,
+    output [7:0] saida
 );
 
-reg [7:0] resultado;
-
-always @(*) begin
-  case (codigo)
-    3'b000: resultado = entrada_A + entrada_B; 
-    3'b001: resultado = entrada_A - entrada_B; 
-    3'b010: resultado = entrada_A;             
-    3'b011: resultado = entrada_B;             
-    default: resultado = 8'b00000000;          
-  endcase
-end
-
-assign saida = resultado;
+assign saida = (codigo == 3'b000) ? 8'b00000000 :
+               (codigo == 3'b001) ? entrada_A :
+               (codigo == 3'b010) ? entrada_B :
+               (codigo == 3'b011) ? (entrada_A + entrada_B) :
+               (codigo == 3'b100) ? (entrada_A - entrada_B) :
+               8'b00000000;
 
 endmodule
+
+
